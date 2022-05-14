76aad0b063134c01be583e9fe544b80050d7781b
Natanael <natanaelsilva.progjs@gmail.com>
2022-05-13 23:30:26 -0300

    refactor: view prodct_detail in shop

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
+from cart.forms import CartAddProductForm
 
 
 
 def product_list(request, category_slug=None):
+
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
@@ -16,5 +18,7 @@ def product_list(request, category_slug=None):
 
 
 def product_detail(request, id, slug):
+
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, 'shop/product/detail.html', {'product': product})
+    
+    return render(request, 'shop/product/detail.html', {'product': product, 'cart_product_form': CartAddProductForm})
