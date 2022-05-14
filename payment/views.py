cc19064463f394dd3be7ab2ae354ea126245e502
Natanael <natanaelsilva.progjs@gmail.com>
2022-05-14 18:03:05 -0300

    feat: payment view

--- payment/views.py
+++ payment/views.py
@@ -41,6 +41,7 @@ def payment_process(request):
         client_token = geteway.client_token.generate()
         return render(request, 'payment/process.html', {'order': order, 'client_token': client_token})
 
+
 def payment_done(request):
     return render(request, 'payment/done.html')
 
