d680a1aaac9871836ee4d647dc2563fc57365606
Natanael <natanaelsilva.progjs@gmail.com>
2022-05-26 16:36:02 -0300

    feat: view detail order in django-admin

--- orders/admin.py
+++ orders/admin.py
@@ -3,6 +3,8 @@ import datetime
 from django.contrib import admin
 from .models import Order, OrderItem
 from django.http import HttpResponse
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 
 
@@ -38,10 +40,15 @@ def export_to_csv(modeladmin, request, queryset):
 export_to_csv.short_description = 'Export to CSV'
 
 
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+
+    return mark_safe('<a href="{}">View</a>'.format(url))
+
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created', 'updated']
+    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
