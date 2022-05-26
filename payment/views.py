b6dd1d484b19e19fa43faee7e9ce5d4bd4a193a2
Natanael <natanaelsilva.progjs@gmail.com>
2022-05-26 17:20:53 -0300

    feat: def payment_completed

--- payment/views.py
+++ payment/views.py
@@ -2,6 +2,7 @@ import braintree
 from django.shortcuts import render, redirect, get_object_or_404
 from django.conf import settings
 from orders.models import Order
+from .tasks import payment_completed
 
 
 geteway = braintree.BraintreeGateway(settings.BRAINTREE_CONF)
@@ -32,6 +33,9 @@ def payment_process(request):
             # store the unique transaction id
             order.braintree_id = result.transaction.id
             order.save()
+
+            payment_completed.delay(order.id)
+
             return redirect('payment:done')
         else:
             return redirect('payment:canceled')
